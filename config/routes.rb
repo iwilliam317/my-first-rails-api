f3e6aa1585e27c3044d5a877c43e1a55c2532cf4
william <willshinji@gmail.com>
2018-04-26 15:42:59 -0300

    api finished

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,16 @@ Rails.application.routes.draw do
   devise_for :users
   namespace :api do
     namespace :v1 do
-      resources :users      
-      resources :languages
+      resources :contacts      
+      # resources :languages
     end    
   end
 
+  # constraints subdomain: 'api' do
+    #   scope module: 'api' do
+    #     namespace :v1 do
+    #       resources :contacts
+    #     end
+    #   end
+    # end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180425224010) do
+ActiveRecord::Schema.define(version: 20180426180200) do
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "phone"
+    t.text "description"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_contacts_on_user_id"
+  end
 
   create_table "languages", force: :cascade do |t|
     t.string "name"
